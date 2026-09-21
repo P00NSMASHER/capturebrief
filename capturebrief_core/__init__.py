58b873309485887339f04d6c33924b676af265c0
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-21 10:33:10 -0400

    Export approved-source adapters

--- capturebrief_core/__init__.py
+++ capturebrief_core/__init__.py
@@ -2,13 +2,19 @@
 from .audit import audit_case
 from .authority import validate_current_action_receipts
 from .history import make_history_receipt, validate_history_receipts
-from .manifest import download_public_resource, fetch_manifest, normalize_manifest_payload, validate_manifest_receipts
+from .manifest import fetch_manifest, normalize_manifest_payload, validate_manifest_receipts
+from .current_api import download_resource_from_api_observation, fetch_latest_active, make_current_action_receipt
+from .data_services import collect_history_from_files, issue_history_receipt_from_snapshots, scan_extract_bytes
+from .source_policy import SourcePolicyError, classify_sam_url, require_approved_automation
 from .packet import build_resource_union, diff_manifest_receipts, validate_reference_closure
 from .render import render_markdown
 from .watch import compare_cases
 
 __all__=[
     "audit_case","validate_current_action_receipts","make_history_receipt","validate_history_receipts",
-    "download_public_resource","fetch_manifest","normalize_manifest_payload","validate_manifest_receipts",
+    "fetch_manifest","normalize_manifest_payload","validate_manifest_receipts",
+    "download_resource_from_api_observation","fetch_latest_active","make_current_action_receipt",
+    "collect_history_from_files","issue_history_receipt_from_snapshots","scan_extract_bytes",
+    "SourcePolicyError","classify_sam_url","require_approved_automation",
     "build_resource_union","diff_manifest_receipts","validate_reference_closure","render_markdown","compare_cases"
 ]
