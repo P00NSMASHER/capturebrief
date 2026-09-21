b815f73c5971b300e3922f61b998379507626152
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-21 12:28:45 -0400

    Test official GSA DITA and embedded clause edition parsing

--- tests/test_rule_registry.py
+++ tests/test_rule_registry.py
@@ -6,12 +6,47 @@ from capturebrief_core.rule_registry import add_rule_version, diff_rule_versions
 DITA='''<?xml version="1.0" encoding="UTF-8"?>
 <concept id="FAR_52_204_21"><title><ph props="autonumber">52.204-21</ph> Basic Safeguarding</title><conbody><p id="a">Contractors shall use safeguards.</p><p id="b">This is a synthetic fixture.</p></conbody></concept>'''
 KW=dict(namespace="FAR",agency="FAR Council",edition="FAC TEST",source_repository="GSA/GSA-Acquisition-FAR",source_revision="a"*40,source_path="dita/52.204-21.dita",source_url="https://github.com/GSA/GSA-Acquisition-FAR/blob/"+"a"*40+"/dita/52.204-21.dita",observed_at="2026-09-21T16:00:00Z")
+OFFICIAL_SHAPED='''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE dita
+  PUBLIC "-//OASIS//DTD DITA Composite//EN" "ditabase.dtd">
+<dita><concept id="FAR_52_204_21">
+<title><ph props="autonumber">52.204-21</ph> Basic Safeguarding of Covered Contractor Information Systems.</title>
+<conbody outputclass="clause">
+<p id="prescription">As prescribed in 4.1903, insert the following clause:</p>
+<p id="heading" outputclass="Ctr_SmCaps">Basic Safeguarding of Covered Contractor Information Systems (Nov 2021)</p>
+<p id="a"><ph props="autonumber">(a)</ph> Contractors shall use safeguards.</p>
+</conbody></concept></dita>'''
 
 class RuleRegistryTests(unittest.TestCase):
     def test_parse_dita(self):
         r=parse_gsa_dita(DITA,**KW); self.assertEqual(r["citation"],"52.204-21"); self.assertEqual(r["rule_key"],"FAR:FAR Council:52.204-21"); self.assertEqual(len(r["paragraphs"]),2); self.assertEqual(validate_rule_source(r),[])
     def test_rejects_entities(self):
         with self.assertRaises(ValueError): parse_gsa_dita('<!DOCTYPE x [<!ENTITY y "z">]><concept><title><ph props="autonumber">1.1</ph>x</title><conbody><p>&y;</p></conbody></concept>',**{**KW,"source_path":"dita/1.1.dita"})
+
+    def test_accepts_exact_gsa_external_dita_doctype(self):
+        kwargs={k:v for k,v in KW.items() if k!="edition"}
+        row=parse_gsa_dita(OFFICIAL_SHAPED,**kwargs)
+        self.assertEqual(row["edition"],"Nov 2021")
+        self.assertEqual(row["edition_basis"],"EMBEDDED_RULE_TEXT")
+        self.assertEqual(row["embedded_edition_locator"],"heading")
+        self.assertTrue(row["standard_doctype_stripped"])
+        self.assertIsNone(row["source_snapshot_label"])
+        self.assertEqual(validate_rule_source(row),[])
+
+    def test_embedded_rule_edition_outranks_source_snapshot_label(self):
+        row=parse_gsa_dita(OFFICIAL_SHAPED,**{**KW,"edition":"FAC 2026-01 snapshot"})
+        self.assertEqual(row["edition"],"Nov 2021")
+        self.assertEqual(row["source_snapshot_label"],"FAC 2026-01 snapshot")
+        self.assertEqual(row["edition_basis"],"EMBEDDED_RULE_TEXT")
+
+    def test_rejects_arbitrary_external_doctype(self):
+        bad=OFFICIAL_SHAPED.replace(
+            'PUBLIC "-//OASIS//DTD DITA Composite//EN" "ditabase.dtd"',
+            'SYSTEM "https://evil.invalid/evil.dtd"',
+        )
+        kwargs={k:v for k,v in KW.items() if k!="edition"}
+        with self.assertRaises(ValueError):
+            parse_gsa_dita(bad,**kwargs)
     def test_commit_is_not_effective_date(self):
         r=parse_gsa_dita(DITA,**KW); self.assertIsNone(r["effective_from"]); self.assertEqual(r["effective_date_authority"],"UNKNOWN"); self.assertFalse(r["applicability_authoritative"])
     def test_registry_versions_are_append_only(self):
